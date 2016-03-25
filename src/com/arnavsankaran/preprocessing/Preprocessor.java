d1e23e5d7f856dcdb02bf9abb02fc928c4903102
ASankaran <arnavsankaran@gmail.com>
2016-03-24 21:06:22 -0500

    Scale images to standard size before preprocessing

--- src/com/arnavsankaran/preprocessing/Preprocessor.java
+++ src/com/arnavsankaran/preprocessing/Preprocessor.java
@@ -2,12 +2,16 @@ package com.arnavsankaran.preprocessing;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+
+import com.arnavsankaran.configuration.Configuration;
 
 public class Preprocessor {
 	
 	private String fileLocation;
+	private Mat source;
 	
 	static {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -15,10 +19,13 @@ public class Preprocessor {
 	
 	public Preprocessor(String fileLocation) {
 		this.fileLocation = fileLocation;
+		source =  Imgcodecs.imread(fileLocation);
+		Mat scaled = new Mat(Configuration.sizeX, Configuration.sizeY, source.type());
+		Imgproc.resize(source, scaled, new Size(Configuration.sizeX, Configuration.sizeY));
+		source = scaled;
 	}
 	
 	public boolean[][] getImageAsBooleanMap() {
-		Mat source =  Imgcodecs.imread(fileLocation);
 		Mat greyscale = new Mat(source.rows(), source.cols(), source.type());
 		Mat binary = new Mat(source.rows(), source.cols(), source.type());
 		Imgproc.cvtColor(source, greyscale, Imgproc.COLOR_BGR2GRAY);
